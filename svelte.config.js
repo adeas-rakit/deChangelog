2082c2fee9b19e22a97d86b2d0d37fc83993f58e
adeas-rakit <ade.arwans@gmail.com>
2025-03-05 14:48:22 +0700

    update deploy script and add trailingSlash configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
- 
-    kit: {
-        adapter: adapter(),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
-        }
-    } 
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+    },
+  },
 };
 
 export default config;
